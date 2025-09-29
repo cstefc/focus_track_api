a62386982ea517ac26d716e98117e8f1e93b8cfe
Stef Ossé <stef.osse@ugent.be>
2025-09-29 15:05:47 +0200

    init project

---
+++ src/main/java/be/osse/activ_planner_api/ActivPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package be.osse.activ_planner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivPlannerApiApplication.class, args);
+	}
+
+}
