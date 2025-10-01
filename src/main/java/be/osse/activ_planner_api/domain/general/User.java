99b3c9601875bc576cfd74de15d105abfd420159
Stef Ossé <stef.osse@ugent.be>
2025-10-01 14:07:14 +0200

    added user entity

--- src/main/java/be/osse/activ_planner_api/domain/general/User.java
+++ src/main/java/be/osse/activ_planner_api/domain/general/User.java
@@ -1,4 +1,70 @@
 package be.osse.activ_planner_api.domain.general;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NaturalId
+    @Column(unique = true,  nullable = false, updatable = false)
+    private String uuid = UUID.randomUUID().toString();
+
+    private String name;
+
+    private String email;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(uuid, user.uuid) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uuid, name, email);
+    }
 }
