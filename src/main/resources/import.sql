370aed9e0ca64f2a5d9538d86f3236596db1e6ae
Stef Ossé <stef.osse@ugent.be>
2025-10-07 10:33:52 +0200

    db connection + mock data setup

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into app_user (id, email, name, uuid) values (1, 'stef@osse.be', 'Stef Ossé', 'cstefc');
+insert into app_user (id, email, name, uuid) values (2, 'john@doe.com', 'John Doe', 'jonny');
