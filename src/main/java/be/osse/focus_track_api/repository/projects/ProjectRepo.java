03769dcb9980827b647d088208c9b72f382a6138
Stef Ossé <stef.osse@ugent.be>
2025-11-10 17:03:46 +0100

    added mock data for development user

--- src/main/java/be/osse/focus_track_api/repository/projects/ProjectRepo.java
+++ src/main/java/be/osse/focus_track_api/repository/projects/ProjectRepo.java
@@ -3,5 +3,8 @@ package be.osse.focus_track_api.repository.projects;
 import be.osse.focus_track_api.domain.projects.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectRepo extends CrudRepository<Project, Long> {
+    List<Project> findByAppUserUuid(String uuid);
 }
