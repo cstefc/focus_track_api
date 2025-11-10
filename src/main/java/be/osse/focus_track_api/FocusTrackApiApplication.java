0f7e98bc0fdf2be4d0921b9d3546dc15bf2bdb9e
Stef Ossé <stef.osse@ugent.be>
2025-11-10 15:28:55 +0100

    small security tweak

--- src/main/java/be/osse/focus_track_api/FocusTrackApiApplication.java
+++ src/main/java/be/osse/focus_track_api/FocusTrackApiApplication.java
@@ -3,11 +3,13 @@ package be.osse.focus_track_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+})
 public class FocusTrackApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FocusTrackApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FocusTrackApiApplication.class, args);
+    }
 
 }
--- src/main/java/be/osse/focus_track_api/security/SecurityConfig.java
+++ src/main/java/be/osse/focus_track_api/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,6 +28,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(firebaseTokenFilter, UsernamePasswordAuthenticationFilter.class)
 
