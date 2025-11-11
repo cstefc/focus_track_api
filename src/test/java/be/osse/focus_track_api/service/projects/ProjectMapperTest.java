ca7a8f9670693b2eb3676f4e72bf9fa5ffbda13d
Stef Ossé <stef.osse@ugent.be>
2025-11-11 20:46:08 +0100

    project mapper test

---
+++ src/test/java/be/osse/focus_track_api/service/projects/ProjectMapperTest.java
@@ -0,0 +1,72 @@
+package be.osse.focus_track_api.service.projects;
+
+import be.osse.focus_track_api.domain.general.AppUser;
+import be.osse.focus_track_api.domain.logging.Log;
+import be.osse.focus_track_api.domain.projects.Project;
+import be.osse.focus_track_api.dto.projects.CreateProjectDTO;
+import be.osse.focus_track_api.dto.projects.GetProjectDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectMapperTest {
+    private ProjectMapper projectMapper;
+
+    @BeforeEach
+    void setUp() {
+        projectMapper = new ProjectMapper();
+    }
+
+    @Test
+    void testToGetProjectDTO() {
+        // GIVEN
+        AppUser appUser = mock(AppUser.class);
+        Project project = mock(Project.class);
+
+        when(project.getId()).thenReturn(420L);
+        when(project.getName()).thenReturn("Test Project");
+        when(project.getDescription()).thenReturn("Test Project");
+        when(project.isArchived()).thenReturn(false);
+
+        when(appUser.getUuid()).thenReturn(UUID.randomUUID().toString());
+        when(project.getUser()).thenReturn(appUser);
+
+
+        // WHEN
+        GetProjectDTO getProjectDTO = projectMapper.toGetProjectDTO(project);
+
+        // THEN
+        assertEquals(420L, getProjectDTO.id());
+        assertEquals(project.getName(), getProjectDTO.name());
+        assertEquals(project.getDescription(), getProjectDTO.description());
+        assertEquals(project.isArchived(), getProjectDTO.archived());
+        assertEquals(project.getUser().getUuid(), getProjectDTO.app_user_uuid());
+    }
+
+    @Test
+    void testToProject() {
+        // GIVEN
+        CreateProjectDTO createProjectDTO = new CreateProjectDTO("name", "desc");
+        AppUser appUser = mock(AppUser.class);
+        Log log = mock(Log.class);
+
+        // WHEN
+        Project result = projectMapper.toProject(createProjectDTO, appUser, log);
+
+        // THEN
+        assertEquals(createProjectDTO.name(), result.getName());
+        assertEquals(createProjectDTO.description(), result.getDescription());
+        assertEquals(appUser, result.getUser());
+        assertEquals(log, result.getLog());
+        assertFalse(result.isArchived());
+        assertNull(result.getId());
+    }
+}
